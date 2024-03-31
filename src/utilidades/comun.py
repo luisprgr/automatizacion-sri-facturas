8b835899b51ece05c225b072d006d7748ead48d5
luisprgr <50538689+luisprgr@users.noreply.github.com>
2024-03-31 00:42:53 -0500

    :necktie: updates ids

--- src/utilidades/comun.py
+++ src/utilidades/comun.py
@@ -1,5 +1,3 @@
-URL_SRI_Y_YO = "https://sriyyoenlinea.sri.gob.ec"
-URL_SRI_Y_YO_LOGIN = URL_SRI_Y_YO + "/portal-sriyyo-internet/pages/inicio.html"
-URL_SRI_Y_YO_FACTURA = (
-    URL_SRI_Y_YO + "/portal-sriyyo-internet/pages/comprobantes/factura/Factura.html"
-)
+URL_SRI_Y_YO = "https://facturadorsri.sri.gob.ec"
+URL_SRI_Y_YO_LOGIN = URL_SRI_Y_YO + "/portal-facturadorsri-internet/pages/inicio.html"
+URL_SRI_Y_YO_FACTURA = URL_SRI_Y_YO + "/portal-facturadorsri-internet/pages/comprobantes/factura/Factura.html"
